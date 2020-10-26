2a2f1ac77ebd90436c90e457d88771f56f6796fd
lollipop <hanggegreat@vip.qq.com>
2020-10-26 19:20:41 +0800

    添加user服务

---
+++ user/src/main/java/cn/charsers/wehappy/UserApplication.java
@@ -0,0 +1,14 @@
+package cn.charsers.wehappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyuanhang
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
