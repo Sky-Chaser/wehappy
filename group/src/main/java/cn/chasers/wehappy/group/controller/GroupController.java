2a2dbc9f39c6157ae2140c8c45d0e3a3fde51547
lollipop <hanggegreat@vip.qq.com>
2020-11-18 17:58:02 +0800

    添加 groupService 用户相关接口

--- group/src/main/java/cn/chasers/wehappy/group/controller/GroupController.java
+++ group/src/main/java/cn/chasers/wehappy/group/controller/GroupController.java
@@ -1,7 +1,14 @@
 package cn.chasers.wehappy.group.controller;
 
 
+import cn.chasers.wehappy.common.api.CommonResult;
+import io.swagger.annotations.ApiParam;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GroupController {
 
+    @GetMapping("/{id}/users")
+    public CommonResult<List<User>> getUsers(@ApiParam(value = "群组Id") @PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping("/{id}/users/{userId}")
+    public CommonResult<User> getUser(@ApiParam(value = "群组Id") @PathVariable Long id, @ApiParam(value = "用户Id") @PathVariable Long userId) {
+        return null;
+    }
 }
 
